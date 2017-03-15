2576770831de2b50890f017d94c99d3af02107d8
Bharadwaaj <bhaaradwaaj@gmail.com>
2017-03-15 09:22:57 +0530

    queue test added

--- Sources/main.c
+++ Sources/main.c
@@ -1,32 +1,13 @@
+
 /*
- * Copyright (c) 2015, Freescale Semiconductor, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of Freescale Semiconductor, Inc. nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+ * ------------------------------------------------------------
+ * "THE BEERWARE LICENSE" (Revision 42):
+ * Bharadwaaj wrote this code. As long as you retain this
+ * notice, you can do whatever you want with this stuff. If we
+ * meet someday, and you think this stuff is worth it, you can
+ * buy me a beer in return.
+ * ------------------------------------------------------------
+*/
 
 #include "SKEAZ1284.h"
 #include "string.h"
@@ -36,36 +17,141 @@
 #include "task.h"
 #include "semphr.h"
 
-/* Demo application include. */
+/* Priorities at which the tasks are created. */
+#define mainQUEUE_RECEIVE_TASK_PRIORITY		( tskIDLE_PRIORITY + 2 )
+#define	mainQUEUE_SEND_TASK_PRIORITY		( tskIDLE_PRIORITY + 1 )
 
-static int i = 0;
-void my_task();
+/* The rate at which data is sent to the queue.  The 200ms value is converted
+to ticks using the portTICK_PERIOD_MS constant. */
+#define mainQUEUE_SEND_FREQUENCY_MS			( 200 / portTICK_PERIOD_MS )
+
+/* The number of items the queue can hold.  This is 1 as the receive task
+will remove items as they are added, meaning the send task should always find
+the queue empty. */
+#define mainQUEUE_LENGTH					( 10 )
+
+/* Values passed to the two tasks just to check the task parameter
+functionality. */
+#define mainQUEUE_SEND_PARAMETER			( 0x1111UL )
+#define mainQUEUE_RECEIVE_PARAMETER			( 0x22UL )
+
+/* The number of the LED that is toggled. */
+#define mainLED_TO_TOGGLE					( 0 )
+/*-----------------------------------------------------------*/
+
+
+/*
+ * The tasks as described in the comments at the top of this file.
+ */
+static void prvQueueReceiveTask( void *pvParameters );
+static void prvQueueSendTask( void *pvParameters );
+
+
+static QueueHandle_t xQueue = NULL;
 int main(void)
 {
 
-    /* Write your code here */
+	/* Create the queue. */
+		xQueue = xQueueCreate( mainQUEUE_LENGTH, sizeof( unsigned long ) );
 
-    /* This for loop should be replaced. By default this loop allows a single stepping. */
-	xTaskCreate( my_task,					/* The function that implements the task. */
-						"task", 									/* The text name assigned to the task - for debug only as it is not used by the kernel. */
+		if( xQueue != NULL )
+		{
+			/* Start the two tasks as described in the comments at the top of this
+			file. */
+			xTaskCreate( prvQueueReceiveTask,					/* The function that implements the task. */
+						"Rx", 									/* The text name assigned to the task - for debug only as it is not used by the kernel. */
 						configMINIMAL_STACK_SIZE, 				/* The size of the stack to allocate to the task. */
-						NULL, /* The parameter passed to the task - just to check the functionality. */
-						(tskIDLE_PRIORITY + 2 ), 		/* The priority assigned to the task. */
-						NULL );
+						( void * ) mainQUEUE_RECEIVE_PARAMETER, /* The parameter passed to the task - just to check the functionality. */
+						mainQUEUE_RECEIVE_TASK_PRIORITY, 		/* The priority assigned to the task. */
+						NULL );									/* The task handle is not required, so NULL is passed. */
 
-	vTaskStartScheduler();
-	while(1);
-    /* Never leave main */
-    return 0;
+			xTaskCreate( prvQueueSendTask, "TX", configMINIMAL_STACK_SIZE, ( void * ) mainQUEUE_SEND_PARAMETER, mainQUEUE_SEND_TASK_PRIORITY, NULL );
+
+			/* Start the tasks and timer running. */
+			vTaskStartScheduler();
+		}
+
+		/* If all is well, the scheduler will now be running, and the following
+		line will never be reached.  If the following line does execute, then
+		there was insufficient FreeRTOS heap memory available for the idle and/or
+		timer tasks	to be created.  See the memory management section on the
+		FreeRTOS web site for more details. */
+		for( ;; );
 }
 
-void my_task()
+
+
+static void prvQueueSendTask( void *pvParameters )
 {
-	while(1)
+TickType_t xNextWakeTime;
+const unsigned long ulValueToSend = 1000UL;
+
+	/* Check the task parameter is as expected. */
+	configASSERT( ( ( unsigned long ) pvParameters ) == mainQUEUE_SEND_PARAMETER );
+
+	/* Initialise xNextWakeTime - this only needs to be done once. */
+	xNextWakeTime = xTaskGetTickCount();
+
+	for( ;; )
 	{
-		;
+		/* Place this task in the blocked state until it is time to run again.
+		The block time is specified in ticks, the constant used converts ticks
+		to ms.  While in the Blocked state this task will not consume any CPU
+		time. */
+		vTaskDelayUntil( &xNextWakeTime, mainQUEUE_SEND_FREQUENCY_MS );
+
+		/* Send to the queue - causing the queue receive task to unblock and
+		toggle the LED.  0 is used as the block time so the sending operation
+		will not block - it shouldn't need to block as the queue should always
+		be empty at this point in the code. */
+		xQueueSend( xQueue, &ulValueToSend, 0U );
 	}
 }
+/*-----------------------------------------------------------*/
+
+static void prvQueueReceiveTask( void *pvParameters )
+{
+unsigned long ulReceivedValue;
+
+	/* Check the task parameter is as expected. */
+	configASSERT( ( ( unsigned long ) pvParameters ) == mainQUEUE_RECEIVE_PARAMETER );
+
+	for( ;; )
+	{
+		/* Wait until something arrives in the queue - this task will block
+		indefinitely provided INCLUDE_vTaskSuspend is set to 1 in
+		FreeRTOSConfig.h. */
+		xQueueReceive( xQueue, &ulReceivedValue, portMAX_DELAY );
+
+		/*  To get here something must have been received from the queue, but
+		is it the expected value?  If it is, toggle the LED. */
+		if( ulReceivedValue == 1000UL )
+		{
+		//	vParTestToggleLED( mainLED_TO_TOGGLE );
+			ulReceivedValue = 0U;
+		}
+	}
+}
+/*-----------------------------------------------------------*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /************* Hooks*****************************/
